f8f52df22a0f1615a7acc6a7275db172a0248200
root <root@manager.c.compute-project-1.internal>
2015-10-31 22:44:32 +0000

    Fix editor

--- Debian-fix.sh
+++ Debian-fix.sh
@@ -2,8 +2,13 @@
 
 #"Debian" "fix" script
 
+# Infinite history
 sed -i -e 's/HISTCONTROL=.*/HISTCONTROL=ignoredups/' /etc/skel/.bashrc
 sed -i -e 's/HISTCONTROL=.*/HISTCONTROL=ignoredups/' ~/.bashrc
+
+# vi editor
+echo "export EDITOR=vi" >> /etc/skel/.bashrc
+echo "export EDITOR=vi" >> ~/.bashrc
 
 # Perhaps a local alternative alias ls='LC_COLLATE=C.UTF-8 ls'
 echo 'LC_COLLATE="C.UTF-8"' >> /etc/default/locale
