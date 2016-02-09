495108875fa43d62b8a040fa3984993ff6776236
Mariusz Woloszyn <emsi@users.noreply.github.com>
2016-02-09 12:08:21 +0100

    Added vimrc via wget

--- Debian-fix.sh
+++ Debian-fix.sh
@@ -10,5 +10,8 @@ sed -i -e 's/HISTCONTROL=.*/HISTCONTROL=ignoredups/' ~/.bashrc
 echo "export EDITOR=vi" >> /etc/skel/.bashrc
 echo "export EDITOR=vi" >> ~/.bashrc
 
+# vimrc file
+wget -qO- https://raw.githubusercontent.com/emsi/fix/master/vimrc > /usr/share/vim/vimrc
+
 # Perhaps a local alternative alias ls='LC_COLLATE=C.UTF-8 ls'
 echo 'LC_COLLATE="C.UTF-8"' >> /etc/default/locale
